8712a7330835d1b818928a93ea4c48b931c73ed1
yutaiwasawa <yutaiwasawa1212@gmail.com>
2025-02-12 16:03:22 +0900

    記事データベース接続確認

--- lib/notion.ts
+++ lib/notion.ts
@@ -8,6 +8,10 @@ if (!process.env.NOTION_CATEGORIES_DATABASE_ID) {
   throw new Error("Missing NOTION_CATEGORIES_DATABASE_ID environment variable");
 }
 
+if (!process.env.NOTION_WORKS_DATABASE_ID) {
+  throw new Error("Missing NOTION_WORKS_DATABASE_ID environment variable");
+}
+
 export const notion = new Client({
   auth: process.env.NOTION_API_KEY,
 });
